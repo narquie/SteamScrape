c85da06490ef9dbf15acb14e4b805065f7d026ee
narquie <nicolas.arquie@gmail.com>
2018-02-24 11:39:35 -0800

    Init data cleaning

--- Scrape_Steam.py
+++ Scrape_Steam.py
@@ -1,17 +1,22 @@
 import urllib2
 import re
 from bs4 import BeautifulSoup
+import time
+import pandas
 # Function that scrapes steam and returns
 # 1) Game name list (game_list)
 # 2) Game hours list (game_hours_list)
 # 3) Friends url (friends_url)
-def scrape_website(overall_dict,queue):
-    games_url_base = "games/?tab=all"
-    friends_url_base = "friends/"
-    games_page = 'http://steamcommunity.com/id/stabstabstab/games/?tab=all'
+def scrape_website(overall_dict,stack):
+    games_url_base = "/games/?tab=all"
+    friends_url_base = "/friends/"
+    games_page = stack[-1]+games_url_base
     page = urllib2.urlopen(games_page)
     soup = BeautifulSoup(page, 'html.parser')
     name_box = soup.find('script',attrs={'language': 'javascript'})
+    if name_box is None:
+        stack.pop()
+        return(overall_dict,stack)
     name = name_box.text
     print(name)
     p = re.compile('"name":"(.*?)"')
@@ -22,38 +27,68 @@ def scrape_website(overall_dict,queue):
     for i in range(0,len(game_hours_list)):
         game_hours_list[i] = float(str(game_hours_list[i]).replace(",",""))
     print(game_hours_list)
-    friends_page = "http://steamcommunity.com/id/stabstabstab/friends/"
+    friends_page = stack[-1]+friends_url_base
     page = urllib2.urlopen(friends_page)
     soup = BeautifulSoup(page, 'html.parser')
     name_box = soup.find("div" ,id="BG_bottom")
+    if name_box is None:
+        stack.pop()
+        return(overall_dict,stack)
     print(name_box)
     name = name_box.find_all('a')
     print(name)
     p = re.compile('href="(.*?)"')
     friends_url = p.findall(str(name))
     print(friends_url)
+    stack.pop()
+    # Start filling the hash table / stack
+    for i in friends_url:
+        if i not in overall_dict:
+            stack.append(i)
+        overall_dict[i] = 1
+    # To not DDOS steam
+    time.sleep(5)
+    return(overall_dict,stack,game_list,game_hours_list)
 
+#INIT
+game_list = []
+game_hours_list = []
 overall_dict = {}
-queue = []
+overall_dict["http://steamcommunity.com/id/stabstabstab"] = 1
+stack = ["http://steamcommunity.com/id/stabstabstab"]
 count = 0
-#while(len(queue)!= 0):
-scrape_website(overall_dict,queue)
-
+cleaned_initial_list = []
+ratio_of_hours = []
+pairs_dict = {}
 # Create  hash table with all urls
-# Call function for each url in queue
-# Add to queue for each friends url if
+# Call function for each url in stack
+# Add to stack for each friends url if
 # friend not in hash table
-# repeat until queue is empty or 10,000
+# repeat until stack is empty or 10,000
 # users have been queried
-
-# Create a ratio of total hours played for a game
-# over sum of total hours played by all games by a
-# user.
-
-# Create 10C2 of top 10 games played and pair them
-# with another game (for 45? combinations). Make a
-# count of the highest paired games
-
-# Organize the data: should be fields [player,game,hoursplayed]
-
-# Make second data source with fields: [player,pairoftop10games]
+while(len(stack)!= 0 and count != 10):
+    overall_dict, stack, game_list,game_hours_list = scrape_website(overall_dict,stack)
+    for i in range(0,min([len(game_list),len(game_hours_list)])):
+        print(i)
+        print(len(game_list))
+        print(len(game_hours_list))
+        # Organize the data: should be fields [player,game,hoursplayed]
+        cleaned_initial_list.append([count,game_list[i],game_hours_list[i]])
+        # Create a ratio of total hours played for a game
+        # over sum of total hours played by all games by a
+        # user.
+        ratio_of_hours.append([count,game_list[i],(game_hours_list[i]/sum(game_hours_list))])
+    # Create 10C2 of top 10 games played and pair them
+    # with another game (for 45? combinations). Make a
+    # count of the highest paired games
+    temp_list = []
+    for i in range(0,10):
+        for j in range(0,10):
+            if (i < j):
+                temp_list = [game_list[i],game_list[j]]
+                temp_list.sort()
+                if (temp_list[0]+temp_list[1]) not in pairs_dict:
+                    pairs_dict[(temp_list[0]+temp_list[1])]=1
+                else:
+                    pairs_dict[(temp_list[0]+temp_list[1])]+=1
+    count += 1
